c7dfbfc894011d9901c11c763f80e0483aa09d49
Akash Mishra <40355162+theamcse@users.noreply.github.com>
2020-09-21 12:51:40 +0530

    MongoDB CURD Operation

---
+++ app.js
@@ -0,0 +1,40 @@
+const mongodb=require('mongodb')
+const MongoClient=mongodb.MongoClient;
+
+const connectionUrl='mongodb://localhost:27017'
+const databaseName='task-manager'
+
+MongoClient.connect(connectionUrl,{useNewUrlParser:true, useUnifiedTopology: true},(error,client)=>{
+    if(error){
+        console.log('Database connectivity error')
+    }
+    else{
+        const db=client.db(databaseName)
+        // const insertMany=db.collection('users').insertOne({
+        //     name:'Akash',
+        //     age:'21'
+        // })
+        // .then((result)=>{
+        //     console.log(result.ops)
+        // })
+        // .catch((error)=>{
+        //     console.log(error)
+        // })
+
+        // const findOneAndDelete=db.collection('users').findOneAndDelete({
+        //     name:'Abhishek'
+        // })
+        // .then((result)=>{
+        //     console.log("Success")
+        // })
+        // .catch((error)=>{
+        //     console.log("Failed")
+        // })
+
+        var find=db.collection('users').find({
+            age:'21'
+        }).toArray((error,result)=>{
+            console.log(result)
+        })
+    }
+})
